08356df5f7f83c289dff7ce49f3e3439ed884dd7
EvanTheSmith <ProfessionalEmail@zoho.com>
2020-07-07 17:03:49 -0500

    Added migration for consoles and ran migrations under RACK_ENV=test

--- db/migrate/20200707215556_create_consoles.rb
+++ db/migrate/20200707215556_create_consoles.rb
@@ -1,4 +1,9 @@
 class CreateConsoles < ActiveRecord::Migration
   def change
+    create_table :consoles do |c|
+      c.string :name
+      c.integer :release_year
+      c.string :manufacturer
+    end
   end
 end
