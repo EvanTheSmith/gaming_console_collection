f91469db66f1bcbb36832a591c0e64e93a859b0f
EvanTheSmith <ProfessionalEmail@zoho.com>
2020-07-08 03:58:19 -0500

    Adding more functionality to login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,25 @@ class UsersController < ApplicationController
       end
     end
 
+    get '/login' do
+      if !logged_in?
+        erb :"users/login"
+      else
+        redirect "/"
+      end
+    end
+
+    post "/login" do
+      binding.pry
+      user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/tweets"
+        else
+        redirect "/login"
+      end
+    end
+
     get "/logout" do
       session.clear
       redirect "/"
