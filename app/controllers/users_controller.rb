0685410b7ec23133a231366175bcffa2715186fb
EvanTheSmith <ProfessionalEmail@zoho.com>
2020-07-08 04:27:16 -0500

    Got login working / fixed typo in signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
         session[:user_id] = user.id
         redirect "/"
       else
-        redirect "users/signup"
+        redirect "/signup"
       end
     end
 
