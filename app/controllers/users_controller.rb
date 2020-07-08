4d09735420b5d81fc40cf70fcb44c862508d6a7c
EvanTheSmith <ProfessionalEmail@zoho.com>
2020-07-08 03:25:08 -0500

    Updated ApplicationController w/ sessions, updated signup.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
     get '/signup' do
         if !logged_in?
           erb :"users/signup"
@@ -6,4 +7,15 @@ class UsersController < ApplicationController
           redirect "index"
         end
     end
+
+    post "/signup" do
+      user = User.new(params[:user])
+      if user.save
+        session[:user_id] = user.id
+        redirect "/index"
+      else
+        redirect "users/signup"
+      end
+    end
+
 end
