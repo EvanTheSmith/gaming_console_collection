b1dd683594d10c45082f85ca47cdf59dce2da444
EvanTheSmith <ProfessionalEmail@zoho.com>
2020-07-09 03:11:29 -0500

    Added slug methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
     has_many :consoles
     validates_presence_of :user_name, :password, :email
     validates_uniqueness_of :user_name, :email
+
+    def slug
+        self.username.parameterize
+      end
+    
+      def self.find_by_slug(slug)
+        self.find{|user| user.slug == slug}
+      end
 end
