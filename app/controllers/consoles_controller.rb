45e675a3323ef805b4ad23bb617d57ec46408a24
EvanTheSmith <ProfessionalEmail@zoho.com>
2020-07-10 18:01:50 -0500

    Added functionality to view individual console, changed links to work with new views

--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -1,4 +1,13 @@
 class ConsolesController < ApplicationController
 
+    get "/consoles" do
+        @consoles = Console.all
+        erb :"consoles/index"
+    end
+
+    get '/consoles/:id' do
+        @console = Console.find(params[:id])
+        erb :"consoles/show"
+    end
 
 end
