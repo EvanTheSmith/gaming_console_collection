83b6aae44d7c50ba431546446570fe3c1c1352c4
EvanTheSmith <ProfessionalEmail@zoho.com>
2020-07-10 16:47:51 -0500

    Reformatted users/show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,8 @@ class ApplicationController < Sinatra::Base
 			!!session[:user_id]
 		end
 
-		def current_user
-			User.find(session[:user_id])
+    def current_user
+      !!session[:user_id] ? User.find(session[:user_id]) : nil
 		end
   end
 
