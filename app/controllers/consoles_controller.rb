4d2b86edd2c3ada06e442dc7a608eb0d570a331d
EvanTheSmith <ProfessionalEmail@zoho.com>
2020-07-11 04:32:49 -0500

    Updated routes, added ability to create new consoles

--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -1,17 +1,31 @@
 class ConsolesController < ApplicationController
 
-    get "/consoles" do
+    get '/consoles' do
         @consoles = Console.all
         erb :"consoles/index"
+    end
+
+    post '/consoles' do
+        console = Console.new(params[:console])
+        console.user = current_user
+        if console.save
+            redirect "/consoles/#{console.id}"
+        else
+            redirect "/consoles/new"
+        end
+    end
+
+    get '/consoles/new' do
+        if logged_in?
+        erb :"consoles/new"
+        else
+        redirect "/login"
+        end
     end
 
     get '/consoles/:id' do
         @console = Console.find(params[:id])
         erb :"consoles/show"
-    end
-
-    get '/consoles/new' do
-        erb :"consoles/new"
     end
 
 end
